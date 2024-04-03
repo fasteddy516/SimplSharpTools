48bc1ef2f074f65c859f58af0ffa299060950899
Edward Wright <fasteddy@thewrightspace.net>
2024-04-03 08:38:53 -0400

    Add version property

--- SimplSharp/SimplSharpTools/Properties/AssemblyInfo.cs
+++ SimplSharp/SimplSharpTools/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimplSharpTools")]
-[assembly: AssemblyDescription("A set of classes to assist with the development of Crestron SIMPL# libraries for use in SIMPL programs visd SIMPL+ modules.")]
+[assembly: AssemblyDescription("A set of classes to assist with the development of Crestron SIMPL# libraries for use in SIMPL programs and SIMPL+ modules.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Edward Wright")]
 [assembly: AssemblyProduct("SimplSharpTools")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion(SimplSharpTools.SimplSharpTools.AssemblyVersion)]
+[assembly: AssemblyFileVersion(SimplSharpTools.SimplSharpTools.AssemblyVersion)]
---
+++ SimplSharp/SimplSharpTools/SimplSharpTools.cs
@@ -0,0 +1,18 @@
+﻿namespace SimplSharpTools
+{
+    /// <summary>
+    /// Provides utility methods and constants for SimplSharpTools.
+    /// </summary>
+    public static class SimplSharpTools
+    {
+        /// <summary>
+        /// The full version of SimplSharpTools used for Assembly attributes.  Build number is always 0.
+        /// </summary>
+        public const string AssemblyVersion = "0.0.1.0";
+
+        /// <summary>
+        /// Gets the offcial version of SimplSharpTools in major.minor.patch format.
+        /// </summary>
+        public static string Version => AssemblyVersion.Substring(0, AssemblyVersion.LastIndexOf('.'));
+    }
+}
